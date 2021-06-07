4eb19465ec1dfcd7ad6ab791508605928f66688e
griesenj <griesenj@mail.gvsu.edu>
2021-06-07 01:04:17 -0400

    Introducing base timer functionality

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import Timer from './utils/Timer';
+import { createStackNavigator } from '@react-navigation/stack';
+
 export default function App() {
+
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={navigatorOptions}>
+        <Stack.Screen name="Timer" component={Timer}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const navigatorOptions = {
+  headerTitleAlign: 'center',
+  headerStyle: {backgroundColor: '#085480'},
+  headerTitleStyle: {color: 'white'}
+};
 
 const styles = StyleSheet.create({
   container: {
---
+++ utils/Timer.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+import Component from 'react';
+
+const Timer = () => {
+
+    const [state, setState] = useState({
+        min: 0, 
+        sec: 0,
+        csec: 0,
+    });
+
+    const [active, setActive] = useState(false);
+    const [timerInterval, setTimerInterval] = useState(null);
+
+    const updateStateObject = (vals) => {
+        setState({
+          ...state,
+          ...vals,
+        });
+      };
+
+    const toggleTimer = () => {
+        if (!active) {
+            console.log('TIMER START');
+            setTimerInterval(setInterval(() => {
+                if (state.csec <= 99) {
+                    console.log('<99');
+                    updateStateObject({csec: state.csec + 1});
+                }
+                 else {
+                    console.log('else');
+                    updateStateObject({csec: 0});
+                }    
+            }, 100));
+            setActive(true);         
+        } else {
+            console.log('TIMER STOP');
+            clearInterval(timerInterval);
+            setActive(false);
+        }
+    }
+
+    function padZeros(number) {
+        return (number < 10) ? "0" + number.toString() : number.toString();
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.timerRow}>
+                <Text style={styles.timerText}>
+                    {padZeros(state.min)}:{padZeros(state.sec)}.{padZeros(state.csec)}
+                </Text>
+            </View>
+            <TouchableOpacity style={styles.button}
+                onPress={() => {
+                    toggleTimer();
+                }}
+                >
+                    <Text style={styles.buttonText}> Start </Text>
+                </TouchableOpacity>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        // alignSelf: 'center',
+        // justifyContent: 'center',
+        // width: "100%",
+    },
+    button: {
+        backgroundColor: 'silver',
+        flex: 1,
+    },
+    timerRow: {
+        alignContent: 'center',
+        backgroundColor: 'black',
+        flex: 1,
+    },
+    timerText: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        color: 'green',
+    },
+    buttonText: {
+        fontSize: 40,
+        justifyContent: 'center',        
+    }
+});
+
+export default Timer;
