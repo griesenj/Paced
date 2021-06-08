b736dde652b60bd1df61bc15d35f11c482a04fc6
griesenj <griesenj@mail.gvsu.edu>
2021-06-08 15:11:03 -0400

    Testing split mechanic

--- screens/TimerScreen.js
+++ screens/TimerScreen.js
@@ -11,14 +11,30 @@ const TimerScreen = () => {
     const [active, setActive] = useState(false);
     const [paused, setPaused] = useState(false);
 
-    const data = [
-        { splitName: 'Sword', splitBest: 1075, activeSplit: null },
-        { splitName: 'Escape', splitBest: 2376, activeSplit: null },
-        { splitName: 'Bottle', splitBest: 3795, activeSplit: null },
-        { splitName: 'Bugs', splitBest: 4580, activeSplit: null },
-        { splitName: 'Wrong Warp', splitBest: 5987, activeSplit: null },
-        { splitName: 'Ganon', splitBest: 6702, activeSplit: null },
-    ];
+    // TESTING SPLIT MECHANIC
+    const [currentSplitValue, setCurrentSplitValue] = useState(0);
+
+    // Need to write setState function that only sets a single value?
+    const [data, setData] = useState([
+        { splitName: 'Sword', splitBest: 1075, splitActive: 937 },
+        { splitName: 'Escape', splitBest: 2376, splitActive: 2000 },
+        { splitName: 'Bottle', splitBest: 3795, splitActive: 3175 },
+        { splitName: 'Bugs', splitBest: 4580, splitActive: 4270 },
+        { splitName: 'Wrong Warp', splitBest: 5987, splitActive: 5980 },
+        { splitName: 'Ganon', splitBest: 6702, splitActive: 6570 },
+        { splitName: 'Sword2', splitBest: 1075, splitActive: 937 },
+        { splitName: 'Escape2', splitBest: 2376, splitActive: 2000 },
+        { splitName: 'Bottle2', splitBest: 3795, splitActive: 3175 },
+        { splitName: 'Bugs2', splitBest: 4580, splitActive: 4270 },
+        { splitName: 'Wrong Warp2', splitBest: 5987, splitActive: 5980 },
+        { splitName: 'Ganon2', splitBest: 6702, splitActive: 6570 },
+        { splitName: 'Sword3', splitBest: 1075, splitActive: 937 },
+        { splitName: 'Escape3', splitBest: 2376, splitActive: 2000 },
+        { splitName: 'Bottle3', splitBest: 3795, splitActive: 3175 },
+        { splitName: 'Bugs3', splitBest: 4580, splitActive: 4270 },
+        { splitName: 'Wrong Warp3', splitBest: 5987, splitActive: 5980 },
+        { splitName: 'Ganon3', splitBest: 6702, splitActive: 6570 },
+    ]);
 
     // TODO: Using custom interval improved accuracy (but rapid pausing might be an issue)
     useEffect(() => {
@@ -69,6 +85,20 @@ const TimerScreen = () => {
         setPaused(false);
     }
 
+    const processSplit = () => {
+        if (active && !paused) {
+            
+            var currentTime = timer;
+            data[currentSplitValue].splitActive = currentTime;
+
+            console.log(data[currentSplitValue].splitActive);
+            
+            console.log(currentTime);
+            setCurrentSplitValue(currentSplitValue + 1);
+        }
+    }
+
+    // TODO: Esnure this accounts for negative numbers (not formatting correctly currently)
     const outputTime = (time) => {
         const hours = Math.floor(time / 36000);
         const minutes = Math.floor(time / 600) % 60;
@@ -89,19 +119,18 @@ const TimerScreen = () => {
 
     const renderSplit = ({item}) => {
         return (
-
             <TouchableHighlight
             activeOpacity={1.0}
             underlayColor={"#ffefd5"}
             >
-                <View>
-                    <Text style={styles.splitText}> {item.splitName} : {outputTime(item.splitBest)} </Text>
+                <View style={{flexDirection: 'row'}}>
+                    <Text style={styles.splitNameText}>{item.splitName}</Text>
+                    <Text style={styles.splitTimeText}> 
+                        {outputTime(item.splitBest)} : {outputTime(item.splitBest - item.splitActive)}
+                          :  {currentSplitValue} | {item.splitActive}
+                    </Text>
                 </View>
-
-
             </TouchableHighlight>
-
-
         )
     }
 
@@ -110,24 +139,20 @@ const TimerScreen = () => {
             <View style={{
                 height: 2,
                 backgroundColor: "#a9a9a9",
-                // width: "98%",
-                // marginLeft: "1%",
             }}/>
         );
     }
 
     return (
         <View style={styles.container}>
+
             <View style={styles.splitsContainer}>
-
-
                 <FlatList
                     ItemSeparatorComponent={renderSeparator}
                     keyExtractor={(item) => item.id}
                     data={data}
                     renderItem={renderSplit}
                 />
-
             </View>
 
             <View style={styles.timerContainer}>
@@ -138,8 +163,13 @@ const TimerScreen = () => {
                     <Text style={styles.timerText}>{outputTime(timer)}</Text>
                 </TouchableOpacity>
 
+                <TouchableOpacity style={{backgroundColor: 'gray', flex: 1, alignItems: 'center'}}
+                    onPress={() => {processSplit()}}
+                >
+                    <Text style={styles.splitButtonText}>Split</Text>
+                </TouchableOpacity>
             </View>
-
+            
         </View>
     )
 };
@@ -158,11 +188,17 @@ const styles = StyleSheet.create({
     },
     splitRow: {
     },
-    splitText: {
+    splitNameText: {
         fontSize: 25,
         fontWeight: 'bold',
         color: 'white',
         padding: 10,
+    },
+    splitTimeText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'white',
+        alignSelf: 'flex-end',
     },
     timerContainer: {
         flex: 1,
@@ -174,7 +210,14 @@ const styles = StyleSheet.create({
         fontSize: 80,
         fontWeight: 'bold',
         color: 'green',
+        backgroundColor: 'white',
     },
+    splitButtonText: {
+        fontSize: 30,
+        color: 'white',
+
+        flexDirection: 'row',
+    }
 });
 
 export default TimerScreen;
